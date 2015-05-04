8e1934ee3c5f4b6672e6518f33d624492ddf57a0
Hackbright Student <info@hackbrightacademy.com>
2015-05-03 18:04:56 -0700

    start of tech talk web app

---
+++ techtalk.py
@@ -0,0 +1,25 @@
+"""Tech talk Flask server."""
+
+from flask import Flask, render_template, redirect, flash, session
+
+import jinja2
+
+app = Flask(__name__)
+
+#app.jinja_env.undefined = jinja2.StructureUndefined
+"""Tells Jinja to make an error if you refer to an undefined 
+    variable in a Jinja template. Helps for debugging."""
+
+@app.route('/')
+def say_hello():
+    return "hello"
+
+@app.route('/homepage')
+def home_page():
+    return render_template('homepage.html')
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
