791d7d27a0390496ca48252b3f430fdd80262e64
Hackbright Student <info@hackbrightacademy.com>
2015-05-05 22:12:29 -0700

    made references html

--- techtalk.py
+++ techtalk.py
@@ -7,20 +7,23 @@ import jinja2
 app = Flask(__name__)
 
 #app.jinja_env.undefined = jinja2.StructureUndefined
-"""Tells Jinja to make an error if you refer to an undefined 
+"""Tells Jinja to make an error if you refer to an undefined
     variable in a Jinja template. Helps for debugging."""
+
 
 @app.route('/')
 def say_hello():
-    return "hello"
-
-@app.route('/homepage')
-def home_page():
     return render_template('homepage.html')
+
 
 @app.route('/how-to-use')
 def how_to_use():
     return render_template('how-to-use.html')
+
+
+@app.route('/references')
+def ref_page():
+    return render_template('references.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
